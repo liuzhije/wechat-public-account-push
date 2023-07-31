89b1280a2c19cb7004604b15c1b20bb26897ea2f
liuzhije <141017640+liuzhije@users.noreply.github.com>
2023-07-31 16:43:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '詹羽熙小姐,早上好',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'wx88681a779bd9a27a',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'cON9UvEoh9FOgzgXWlIVfYi4-vk3-tdD-IMjSJvDYis',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oGzgl6U68yIBiBMJ6e0nQpU3zi-A',
     }
   ],
 
